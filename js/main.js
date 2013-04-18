d26ab37b139c1e9a85c50a4ee6cdeac2d8f51a86
Jasper Zonneveld <jasper.zonneveld@gmail.com>
2013-04-18 22:33:24 +0200

    Simple version showing annotations

---
+++ js/main.js
@@ -0,0 +1,65 @@
+var pop;
+
+$(function(){
+	// create our popcorn instance
+	pop = Popcorn( "#video" );
+	
+	// set our defaults
+	pop.defaults( "annotation", {
+	  // set a default element target id and onclick function
+	  target: "annotations",
+	  onclick: function(e, options) {
+	  	aAnnotations = (localStorage[sLocalStorageKey])? JSON.parse(localStorage[sLocalStorageKey]) : {};
+	  	aAnnotations[options.id] = options.label;
+	  	markChosen(options.id);
+		localStorage[sLocalStorageKey] = JSON.stringify(aAnnotations);
+	  }
+	});
+	
+	getAnnotations();
+});
+
+function getAnnotations(data) {
+	var data = data||{};
+
+	data.mediaresource = sMediaresource;
+	data.startTime = 2318.84;
+	data.endTime = 2325.52;
+	
+	$.ajax(sAnnotationsURL, {
+		 dataType: "json",
+		 type: "GET",
+		 data: data,
+		 error: function(e) {console.log(e);},
+		 success: function(o) {
+			// add annotations
+			$(o.annotations).each(function(i,e) {
+				pop.annotation({
+				  annotation: e.annotation,
+				  start: e.startTime,
+				  end: e.endTime,
+				  label: e.label
+				});
+			});
+			
+			// play video
+			pop.play();
+			  
+			// parse localStorage
+			parseLocalStorage(sLocalStorageKey);
+		}
+	});
+}
+
+function markChosen(id) {
+	document.getElementById('annotation-'+id).className += " chosen";
+}
+
+function parseLocalStorage(key){
+	if (localStorage[key]) {
+		aAnnotations = JSON.parse(localStorage[key]);
+		for (var key in aAnnotations) {
+			markChosen(key);
+		}
+	}
+}
---
+++ js/popcorn.annotation.js
@@ -0,0 +1,160 @@
+// PLUGIN: Annotation
+(function ( Popcorn ) {
+
+  /**
+     * annotation popcorn plug-in for LinkedTV
+     * Adds an annotation associated with a certain time in the video, which creates a scrolling view of each item as the video progresses
+     * Options parameter will need a start, target, id and label
+     * -Start is the time that you want this plug-in to execute
+     * -End is the time that you want this plug-in to stop executing, tho for this plugin an end time may not be needed ( optional )
+     * -Target is the id of the DOM element that you want the annotations to appear in. This element must be in the DOM
+     * -Annotation is the identifier of the current annotation
+     * -Label is the content of the current annotation
+     * -Direction specifies whether the timeline will grow from the top or the bottom, receives input as "UP" or "DOWN" ( optional )
+     * -onclick is a function or name of function that will be executed when the annotation is clicked. Parameters given are the click event and the options object ( optional )
+     * @param {Object} options
+     *
+     * Example:
+      var p = Popcorn("#video")
+        .annotation( {
+         start: 1682.8, // seconds
+         target: "annotations",
+         annotation: "http://data.linkedtv.eu/annotation/ccecd104-a558-4ef4-ba17-779688a5720d",
+         label: "Nederland"
+      } )
+    *
+  */
+
+  var i = 1;
+
+  Popcorn.plugin( "annotation" , function( options ) {
+	  
+    options.id = options.annotation.match(/(?:[\w-](?!\/))+$/i);
+
+    var target = document.getElementById( options.target ),
+        contentDiv = document.createElement( "div" ),
+        container,
+        goingUp = true;
+
+    if ( target && !target.firstChild ) {
+      target.appendChild ( container = document.createElement( "div" ) );
+      container.style.width = "inherit";
+      container.style.height = "inherit";
+      container.style.overflow = "auto";
+    } else {
+      container = target.firstChild;
+    }
+
+    contentDiv.style.display = "none";
+    contentDiv.id = "annotation-" + options.id;
+    contentDiv.className = "annotation";
+
+    //  Default to up if options.direction is non-existant or not up or down
+    options.direction = options.direction || "up";
+    if ( options.direction.toLowerCase() === "down" ) {
+
+      goingUp = false;
+    }
+
+    if ( target && container ) {
+      // if this isnt the first div added to the target div
+      if( goingUp ){
+        // insert the current div before the previous div inserted
+        container.insertBefore( contentDiv, container.firstChild );
+      }
+      else {
+
+        container.appendChild( contentDiv );
+      }
+
+    }
+    
+    // convert function name reference to function
+    if (typeof(options.onclick) == "string") {
+    	options.onclick = window[options.onclick];
+    }
+
+    i++;
+    
+    annotationElement = document.createElement( "span" );
+    annotationElement.innerHTML = options.label;
+    if (typeof(options.onclick) == "function") {
+	    annotationElement.onclick = function(e) {
+	    	try {
+	    		options.onclick(e, options);
+	    	} catch (error) {
+	    		console.log(error);
+	    	}
+	    };
+    }
+
+    contentDiv.appendChild(annotationElement);
+
+    return {
+
+      start: function( event, options ) {
+        contentDiv.style.display = "";
+
+        if( options.direction === "down" ) {
+          container.scrollTop = container.scrollHeight;
+        }
+      },
+
+      end: function( event, options ) {
+        contentDiv.style.display = "none";
+      },
+
+      _teardown: function( options ) {
+
+        ( container && contentDiv ) && container.removeChild( contentDiv ) && !container.firstChild && target.removeChild( container );
+      }
+    };
+  },
+  {
+
+    about: {
+      name: "Popcorn Annotation Plugin",
+      version: "0.2",
+      author: "Jasper Zonneveld",
+      website: "jasper.zonneveld.me"
+    },
+
+    options: {
+      start: {
+        elem: "input",
+        type: "number",
+        label: "Start"
+      },
+      end: {
+        elem: "input",
+        type: "number",
+        label: "End",
+        optional: true
+      },
+      target: "feed-container",
+      annotation: {
+        elem: "input",
+        type: "text",
+        label: "Identifier"
+      },
+      label: {
+        elem: "input",
+        type: "text",
+        label: "Label"
+      },
+      direction: {
+        elem: "select",
+        options: [ "DOWN", "UP" ],
+        label: "Direction",
+        optional: true
+      },
+      onclick: {
+        elem: "input",
+        type: "text",
+        label: "OnClick",
+        optional: true
+      }
+    }
+  });
+
+})( Popcorn );
